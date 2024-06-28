19652018a7034fe26b1dff5e24fc2c45f09653c7
Juan Camilo <162502085+Camilo-Dev1975@users.noreply.github.com>
2024-06-27 23:24:11 -0500

    list folder structure, complete product-listing.js

---
+++ scripts/product-listing.js
@@ -0,0 +1,228 @@
+const product1_row1 = {
+    id: 1,
+    nombre:"Luxury Gems Necklace",
+    codigo: "A1",
+    precio: 168.76,
+    tipoDeAccesorio:"collar",
+    imagen: "/images/imagenes-listing-jhon/loxury.jpg",
+    descripcion:`Este collar de diamantes cuadrado es una joya elegante y sofisticada que atraerá todas las miradas. Con un diamante central rodeado por un engaste de oro blanco, su diseño minimalista y moderno lo convierte en el complemento perfecto para cualquier ocasión. Con su brillo deslumbrante y diseño atemporal, es una elección excepcional para añadir un toque de lujo a tu estilo.`,
+    stock: 60
+};
+
+const product2_row1 = {
+    id: 2,
+    nombre:"Aurora Ring",
+    codigo: "A2",
+    precio: 125.28,
+    tipoDeAccesorio:"Brazalete",
+    imagen: "/images/imagenes-listing-jhon/aurora.jpg",
+    descripcion: `Presentamos una elegante pulsera de oro con incrustaciones de diamantes, perfecta para cualquier ocasión especial. El brillo de los diamantes resalta la belleza del oro, creando un contraste deslumbrante. Ideal para quienes buscan un toque de lujo y distinción en su estilo.`,
+    stock: 52 
+};
+
+const product3_row1 = {
+    id: 3,
+    nombre: "Reflections Necklace",
+    codigo: "A3",
+    precio: 620.73,
+    tipoDeAccesorio: "pulsera",
+    imagen: "/images/imagenes-listing-jhon/reflections.jpg",
+    descripcion: `Este brazalete elegante y sofisticado tiene una cadena de oro y una cuenta roja y negra, resaltando su belleza única. Es el complemento perfecto para cualquier ocasión, ya sea una noche especial o para agregar estilo a tu atuendo diario. Llamará la atención y te hará lucir deslumbrante.`,
+    stock: 25
+};
+
+const product4_row1 = {
+    id: 4,
+    nombre: "Dreamy Infinity Ring",
+    codigo: "A4",
+    precio: 327.71,
+    tipoDeAccesorio: "Anillo",
+    imagen: "/images/imagenes-listing-jhon/dreamy.jpg",
+    descripcion: `Este anillo de compromiso tiene un zafiro rosa rodeado de diamantes, creando una combinación elegante y romántica. El zafiro rosa es la piedra central, conocida por su belleza y simbolismo de amor y compromiso. Los diamantes añaden brillo y realzan la belleza del zafiro. Con un diseño clásico y atemporal, este anillo es perfecto para aquellos que buscan una pieza única y significativa para sellar su amor eterno.`,
+    stock: 52
+};
+
+/* Segunda Linea */
+
+const product1_row2 = {
+    id: 5,
+    nombre: "Opulent Jewels Ring",
+    codigo: "A5",
+    precio: 168.76,
+    tipoDeAccesorio: "Anillo",
+    imagen: "/images/imagenes-listing-jhon/opulent.jpg",
+    descripcion: `Este anillo de oro y plata tiene un diseño elegante y sofisticado, perfecto para cualquier ocasión. Hecho con materiales de alta calidad y acabado pulido para un brillo deslumbrante, es el accesorio ideal para agregar estilo y elegancia a cualquier atuendo.`,
+    stock: 87
+};
+
+const product2_row2 = {
+    id: 6,
+    nombre: "Serene Solitaire Earrings",
+    codigo: "A6",
+    precio: 125.28,
+    tipoDeAccesorio: "Aretes",
+    imagen: "/images/imagenes-listing-jhon/serene.jpg",
+    descripcion: `Estos hermosos aretes en forma de corazón son el accesorio perfecto para añadir un toque de elegancia a cualquier atuendo. Confeccionados en un vibrante tono azul, estos aretes destacan por su diseño único y romántico.`,
+    stock: 13
+};
+
+const product3_row2 = {
+    id: 7,
+    nombre: "Timeless Halo Earrings",
+    codigo: "A7",
+    precio: 620.73,
+    tipoDeAccesorio: "Aretes",
+    imagen: "/images/imagenes-listing-jhon/timeles.jpg",
+    descripcion: `Estos pendientes de diamantes en oro blanco de 18 quilates son una elección elegante y atemporal para cualquier ocasión.`,
+    stock: 87
+};
+
+const product4_row2 = {
+    id: 8,
+    nombre: "Exquisite Earrings",
+    codigo: "A8",
+    precio: 327.71,
+    tipoDeAccesorio: "Aretes",
+    imagen: "/images/imagenes-listing-jhon/exquisite.jpg",
+    descripcion: `Estos elegantes aretes en forma de lágrima combinan a la perfección el verde y el dorado, creando una pieza única y llamativa que complementará cualquier atuendo con un toque de sofisticación y estilo. Ideal para ocasiones especiales o para darle un toque de color a tu look diario, estos aretes son una excelente adición a cualquier colección de joyas.`,
+    stock: 30
+};
+
+/* Producto Big*/
+
+const product_big = {
+    id: 9,
+    nombre: "Timeless Elegance Ring",
+    codigo: "A9",
+    precio: 168.76,
+    tipoDeAccesorio: "Anillo",
+    imagen: "/images/imagenes-listing-jhon/timeles elegance.jpg",
+    descripcion: `Estos dos anillos están elaborados con zafiros azules y diamantes, creando una combinación elegante y sofisticada, el diseño clásico y atemporal de estas joyas las convierte en piezas perfectas para cualquier ocasión, ya sea para uso diario o para eventos especiales.`,
+    stock: 5
+}
+
+/* Linea 3 */
+
+const product1_row3 = {
+    id: 10,
+    nombre: "Luxury Charms Ring",
+    codigo: "A10",
+    precio: 620.73,
+    tipoDeAccesorio: "Anillo",
+    imagen: "/images/imagenes-listing-jhon/laxury charms.jpg",
+    descripcion: `Este anillo con una piedra naranja y cadena de oro es el accesorio perfecto para agregar un toque de color y elegancia a cualquier atuendo, La piedra naranja resalta sobre el brillo del oro, creando un contraste llamativo y sofisticado, Ideal para ocasiones especiales o para uso diario, este anillo es una pieza versátil que seguramente llamará la atención.`,
+    stock: 7 
+}
+
+const product2_row3 = {
+    id: 11,
+    nombre: "Blissful Bloom Ring",
+    codigo: "A11",
+    precio: 620.73,
+    tipoDeAccesorio: "Anillo",
+    imagen: "/images/imagenes-listing-jhon/blissful.jpg",
+    descripcion: `Anillo de oro con detalles de hojas, perfecto para agregar un toque de elegancia y naturaleza a cualquier atuendo. Este anillo único está diseñado con un estilo delicado y sofisticado, ideal para aquellos que buscan una pieza de joyería que se destaque.`,
+    stock: 21
+}
+
+/* Linea 4 */
+
+const product1_row4 = {
+    id: 12,
+    nombre: "Sparkling Ring",
+    codigo: "A12",
+    precio: 620.73,
+    tipoDeAccesorio: "Anillo",
+    imagen: "/images/imagenes-listing-jhon/sparkling ring.jpg",
+    descripcion: `Este anillo de diamantes ovalado con un engaste de halo es una joya elegante y sofisticada. El diamante ovalado central brilla intensamente rodeado por una serie de pequeños diamantes que realzan su belleza` ,
+    stock: 79
+}
+
+const product2_row4 = {
+    id: 13,
+    nombre: "Glimmering Ring",
+    codigo: "A13",
+    precio: 620.73,
+    tipoDeAccesorio: "Anillo",
+    imagen: "/images/imagenes-listing-jhon/glimmering.jpg",
+    descripcion: `Este anillo de diamantes en oro rosa de 14 quilates es una joya elegante y sofisticada. El diseño de banda delgada resalta la belleza de los diamantes incrustados en la pieza. El oro rosa le da un toque moderno y femenino, mientras que los diamantes añaden un brillo deslumbrante. Perfecto para cualquier ocasión, este anillo es una elección atemporal y lujosa.`,
+    stock: 52 
+}
+
+const products = [
+
+    product1_row1,
+    product2_row1,
+    product3_row1,
+    product4_row1,
+    product1_row2,
+    product2_row2,
+    product3_row2,
+    product4_row2,
+    product_big,
+    product1_row3,
+    product2_row3,
+    product1_row4,
+    product2_row4
+];
+
+function filterArrayForType (typeOfProduct) {
+
+    const filteredByType = products.filter(producto => producto.tipoDeAccesorio.toLowerCase() == typeOfProduct.toLowerCase());
+    console.log(filteredByType);
+
+}
+
+function filterArrayForName(nameOfProduct) {
+
+
+    const filteredByName = products.filter(producto => producto.nombre.toLowerCase().includes(nameOfProduct.toLowerCase()));
+    console.log(filteredByName);
+
+}
+
+function filterArrayForPriceAsce() {
+
+    const filteredByPrice = products.slice().sort((a, b) => a.precio- b.precio);
+    console.log(filteredByPrice);
+
+}
+
+function filterArrayForPriceDesc() {
+    const filteredByPrice2 = products.slice().sort((a, b) => b.precio - a.precio);
+    console.log(filteredByPrice2);
+}
+
+function totalToPay(productosToPay) {
+
+    const totalReduce = productosToPay.reduce((total, producto) => {
+        
+        return total + (producto.cantidad * producto.precioUnitario);
+    }, 0);
+
+/* Esta parte esta aqui por que no he encontrado una solucion para que se ejecute cuando el 
+cliente lo desee pero va fuera de la funcion, Igualmente ya le estoy buscando una solucion
+(Lo que estoy diciendo solo aplica de aqui para abajo) */
+
+    console.log( totalReduce);
+
+    let productosCompra = [
+        { id: 1, cantidad: 2, precioUnitario: 168.76 },
+        { id: 2, cantidad: 1, precioUnitario: 125.28 }
+    ]
+    let totalCompra = totalToPay(productosCompra);
+    
+    console.log("Total a pagar de la compra: " + totalCompra);
+}
+
+
+
+
+
+
+
+
+
+
+
+
